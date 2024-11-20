764050692ab82c83995ee1d9aa03a800f2b9cd34
Choice148 <137381455+Choice148@users.noreply.github.com>
2024-11-20 22:01:35 +0100

    Update Home.js Remove Marque

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,20 +36,12 @@ function Home() {
   };
   return (
     <>
-      <Marquee />
+      // <Marquee /> no need
       <Header />
       <HowItWorks />
       <Footer />
 
-      {/* <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form> */}
+      
     </>
   );
 }
