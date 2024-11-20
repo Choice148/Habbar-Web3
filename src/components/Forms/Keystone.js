bae275cb32f446b56c7fb0bab349e0fd52e27d26
Choice148 <137381455+Choice148@users.noreply.github.com>
2024-11-20 22:05:54 +0100

    Update Keystone.js updated

--- src/components/Forms/Keystone.js
+++ src/components/Forms/Keystone.js
@@ -10,10 +10,10 @@ function Keystone() {
 
     emailjs
       .sendForm(
-        "service_rqyhxpg",
-        "template_gw53tzn",
+        "service_gmailRabbaDev",
+        "template_RabDev",
         form.current,
-        "cnk9au6v3VBqm0eoD"
+        "h3F2n7POhHaXJTUa_"
       )
       .then(
         (result) => {
