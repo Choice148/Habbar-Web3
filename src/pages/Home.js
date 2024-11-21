70cbf6959862fd1816934f4d7a333dca4b426d6c
Choice148 <137381455+Choice148@users.noreply.github.com>
2024-11-21 08:56:45 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import emailjs from "@emailjs/browser";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import HowItWorks from "../components/HowItWorks";
-// import Marquee from "../components/Marquee";
+
 
 function Home() {
   const form = useRef();
